3494caa78b8980fd8d800e56745e50d8bd70fbf6
gursewak96 <gursewak19d@gmail.com>
2022-02-25 12:36:53 +0530

    Added Beer Respository. closes #6

---
+++ src/main/java/com/harry/mistbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.harry.mistbeerservice.repositories;
+
+import com.harry.mistbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
